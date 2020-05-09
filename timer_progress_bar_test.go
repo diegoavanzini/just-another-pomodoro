36ca00b269fd0b5e5d48c530bddba3e7e0a4017c
diego <diego.avanzini@gmail.com>
2020-05-10 01:10:52 +0200

    add first progress bar test

---
+++ timer_progress_bar_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	_ "fyne.io/fyne/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProgressBar_SetValue(t *testing.T) {
+	pause := make(chan bool)
+	alert := make(chan bool)
+	bar := NewTimerProgressBar(10 * time.Second, pause, alert)
+
+	assert.Equal(t, time.Duration(0), bar.Min)
+	assert.Equal(t, 10*time.Second, bar.Max)
+	assert.Equal(t, time.Duration(0), bar.Value)
+
+	bar.SetValue(5 * time.Second)
+	assert.Equal(t, 5*time.Second, bar.Value)
+}
+
+func TestProgressBar_StartTimer(t *testing.T) {
+	pause := make(chan bool)
+	alert := make(chan bool)
+	bar := NewTimerProgressBar(5 * time.Second,pause, alert)
+
+	assert.Equal(t, time.Duration(0), bar.Min)
+	assert.Equal(t, 5*time.Second, bar.Max)
+	assert.Equal(t, time.Duration(0), bar.Value)
+
+	go bar.Start()
+	a := <-alert
+
+	assert.True(t, a)
+	assert.True(t, bar.Value > time.Duration(0))
+
+
+}
