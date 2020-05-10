e639c11b307632814b02849edd83e6094b584980
diego <diego.avanzini@gmail.com>
2020-05-10 09:23:10 +0200

    modify title setting

--- main.go
+++ main.go
@@ -10,34 +10,37 @@ import (
 	"image/color"
 )
 
-
 const title = "just another pomodoro"
 
 func main() {
 	pomodoro := app.New()
 	pomodoroWindows := pomodoro.NewWindow(title)
 
+	pause := make(chan bool, 2)
 	alert := make(chan bool)
-	go func(alert chan bool) {
+	go func(pause, alert chan bool) {
 		for {
-			<-alert
-			pomodoroWindows.RequestFocus()
+			select {
+			case <-alert:
+				pomodoroWindows.RequestFocus()
+			case paused := <-pause:
+				if paused {
+					pomodoroWindows.SetTitle(title + " (paused)")
+				} else {
+					pomodoroWindows.SetTitle(title)
+				}
+			}
 		}
-	}(alert)
+	}(pause, alert)
+
 	pomodoroWindows.Resize(fyne.Size{420, 70})
 	winGrid := fyne.NewContainerWithLayout(layout.NewVBoxLayout())
 
-	pause := make(chan bool, 2)
 	addPomodoro := make(chan bool)
 	buttonsContainer := container.NewButtonContainer(pause)
 	progressBarContainer := container.NewProgressBarContainer(pause, alert, addPomodoro)
 
-	doneContainer := container.NewPomodoroDoneContainer(layout.NewHBoxLayout())
-	doneContainer.AddPomodoro()
-	dailyContainer := fyne.NewContainerWithLayout(
-		layout.NewVBoxLayout(),
-		canvas.NewRectangle(color.White),
-		doneContainer.Container)
+	dailyContainer := NewDailyPomodoroContainer(addPomodoro)
 
 	progressAndDoneContainer := fyne.NewContainerWithLayout(
 		layout.NewVBoxLayout(),
@@ -50,22 +53,30 @@ func main() {
 		progressAndDoneContainer,
 		buttonsContainer.Container)
 
+	winGrid.AddObject(timerContainer)
+
+	pomodoroWindows.SetContent(winGrid)
+	pomodoroWindows.ShowAndRun()
+}
+
+func NewDailyPomodoroContainer(addPomodoro chan bool) *fyne.Container {
+	doneContainer := container.NewPomodoroDoneContainer(layout.NewHBoxLayout())
+	doneContainer.AddPomodoro()
+
+	dailyContainer := fyne.NewContainerWithLayout(
+		layout.NewVBoxLayout(),
+		canvas.NewRectangle(color.White),
+		doneContainer.Container,
+		canvas.NewRectangle(color.White))
+
 	go func(pDone *container.PomodoroDoneContainer) {
 		for {
 			select {
 			case <-addPomodoro:
 				pDone.AddPomodoro()
-			case paused :=<-pause:
-				if paused {
-					pomodoroWindows.SetTitle(title + " (paused)")
-				} else {
-					pomodoroWindows.SetTitle(title)
-				}
 			}
 		}
 	}(doneContainer)
-	winGrid.AddObject(timerContainer)
 
-	pomodoroWindows.SetContent(winGrid)
-	pomodoroWindows.ShowAndRun()
+	return dailyContainer
 }
